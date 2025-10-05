0dcb921ed2a5865f4cc92d317862950d35135224
leakrullp <leakrull96@gmail.com>
2025-10-05 19:42:03 +0200

    Refactor to google apps script2

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ document
     e.preventDefault();
 
     const formData = new FormData(e.target);
-    const data = {
-      name: formData.get("name"),
-      email: formData.get("email"),
-      country: formData.get("country"),
-      message: formData.get("message"),
-      visible: formData.get("visible") === "yes" ? "yes" : "no",
-    };
+    // const data = {
+    //   name: formData.get("name"),
+    //   email: formData.get("email"),
+    //   country: formData.get("country"),
+    //   message: formData.get("message"),
+    //   visible: formData.get("visible") === "yes" ? "yes" : "no",
+    // };
 
     try {
       await fetch(
         "https://script.google.com/macros/s/AKfycbxaqD55Iv-a3bKLE4DMVBmcTd0E1usjZmJ45nlABJ3UOMH4FiRyaKQSpEjzeArTOnEM/exec",
         {
           method: "POST",
-          mode: "no-cors",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(data).toString(),
+          body: formData, // 👈 send as multipart/form-data
         }
       );
 
